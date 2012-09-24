ad69f0423bfa4b891d1950336800f6e2cff0f58a
davilla <davilla@4pi.com>
2012-09-24 12:29:25 -0400

    [Libdvdread] Add libdvdread hack to force ignore of /root (aml specific)

--- lib/libdvd/libdvdread/src/dvd_reader.c
+++ lib/libdvd/libdvdread/src/dvd_reader.c
@@ -250,6 +250,7 @@ static dvd_reader_t *DVDOpenImageFile( const char *location, int have_css )
         dvdinput_close(dev);
         return NULL;
     }
+    memset( dvd, 0, sizeof( dvd_reader_t ) );
     dvd->isImageFile = 1;
     dvd->dev = dev;
     dvd->path_root = NULL;
@@ -516,6 +517,8 @@ dvd_reader_t *DVDOpen( const char *ppath )
 	    fclose( mntfile );
 	}
 #elif defined(__linux__)
+    /* amlogic/linux hack, tries to use /root as a device, wtf? */
+    if (strcmp("/root", path_copy )) {
         mntfile = fopen( _PATH_MOUNTED, "r" );
         if( mntfile ) {
             struct mntent *me;
@@ -534,6 +537,7 @@ dvd_reader_t *DVDOpen( const char *ppath )
             }
             fclose( mntfile );
 	}
+    }
 #elif defined(_WIN32) || defined(__OS2__)
 #ifdef __OS2__
     /* Use DVDOpenImageFile() only if it is a drive */
